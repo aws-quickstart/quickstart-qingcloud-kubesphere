717d0bcbc75e9edd11c5e617632f9235a18e17ab
troy-ameigh <ameighta@amazon.com>
2020-11-19 17:10:26 -0600

    testing new doc process

---
+++ docs/boilerplate/.utils/build_docs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+ASCIIDOC_ATTRIBUTES=""
+GITHUB_REPO_OWNER=$(echo ${GITHUB_REPOSITORY} | cut -d '/' -f 1)
+if [ -d docs/images ]; then
+  mv docs/images images
+fi
+if [ "${GITHUB_REPO_OWNER}" == "aws-quickstart" ]; then
+  cp docs/boilerplate/.css/AWS-Logo.svg images/
+  if [ "${GITHUB_REF}" == "refs/heads/master" ] || [ "${GITHUB_REF}" == "refs/heads/main" ]; then
+    ASCIIDOC_ATTRIBUTES="-a production_build"
+  fi
+fi
+asciidoctor --base-dir docs/ --backend=html5 -o ../index.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs/boilerplate/index.adoc
+
+
+if [ -d docs-languages ]; then
+  for dir in docs-languages/*/
+  do
+    dir=${dir%*/}
+    lang=$(echo ${dir%*/} | awk -F'[-]' '{print $2}')
+    asciidoctor --base-dir docs-languages/${dir##*/}/ --backend=html5 -o ../index-${lang}.html -w --failure-level ERROR --doctype=book -a toc2 ${ASCIIDOC_ATTRIBUTES} docs-languages/${dir##*/}/boilerplate/index.adoc
+  done
+fi
